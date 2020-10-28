613388031302082ee972e0fe1156528dc170d74a
Colby Roberts <colbiverse@gmail.com>
2020-10-27 22:20:00 -0700

    mad

--- src/biological_neuron.py
+++ src/biological_neuron.py
@@ -10,6 +10,8 @@ class BiologicalNeuron:
         self.initial_v, self.initial_n, self.initial_m, self.initial_h = 0, 1/(1.25 * (math.e - 1) + 1), 2.5 / (2.5 + 4 * (math.e ** 2.5 - 1)), self.x/(self.x + 1)
         self.initial_positions = (0, (self.initial_v, self.initial_n, self.initial_m, self.initial_h))
         self.C, self.V_Na, self.V_k, self.V_L, self.g_bar_Na, self.g_bar_k, self.g_bar_l = 1, 115, -12, 10.6, 120, 36,  0.3
+        self.child = None
+        self.parent = None
 
     def plot_activity(self):
         euler = EulerEstimator(self.derivatives, self.initial_positions)
