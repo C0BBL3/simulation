ef283500c21ed1d0ffbe029dfc4b2c75699d7497
Colby Roberts <colbiverse@gmail.com>
2020-10-27 23:02:44 -0700

    push 2

--- src/biological_neural_network.py
+++ src/biological_neural_network.py
@@ -1,28 +1,34 @@
 class BiologicalNeuralNetwork:
-	def __init__(self, neurons, synapses):
-		self.neurons = neurons
-		self.synapses = synapses
-		for neuron_1_index, neuron_2_index in self.synapses: 
+    def __init__(self, neurons, synapses):
+        self.neurons = neurons
+        self.synapses = synapses
+        for neuron_1_index, neuron_2_index in self.synapses:
             self.neurons[neuron_1_index].child = self.neurons[neuron_2_index]
 
-	def get_derivatives(self):
-		derivatives, current_neuron, neuron_index = [], self.neurons[0], 0
-		while True:
-			num_derivatives = len(current_neuron.derivatives)
-			for derivative_index, derivative in enumerate(current_neuron.derivatives):
-				if derivative_index == 0 and neuron_index > 0: derivatives.append(
-                    lambda t,x, neuron_index=neuron_index, derivative=derivative:
-                        derivative(t,x[num_derivatives*neuron_index:num_derivatives*neuron_index+num_derivatives])
+    def get_derivatives(self):
+        derivatives, current_neuron, neuron_index = [], self.neurons[0], 0
+        while True:
+            num_derivatives = len(current_neuron.derivatives)
+            for derivative_index, derivative in enumerate(current_neuron.derivatives):
+                if derivative_index == 0 and neuron_index > 0:
+                    derivatives.append(
+                        lambda t, x, neuron_index=neuron_index, derivative=derivative:
+                        derivative(
+                            t, x[num_derivatives*neuron_index:num_derivatives*neuron_index+num_derivatives])
                         + x[num_derivatives*(neuron_index-1)]
                     )
-				else: derivatives.append(
-                    lambda t,x, neuron_index=neuron_index, derivative=derivative:
-                        derivative(t,x[num_derivatives*neuron_index:num_derivatives*neuron_index+num_derivatives])
+                else:
+                    derivatives.append(
+                        lambda t, x, neuron_index=neuron_index, derivative=derivative:
+                        derivative(
+                            t, x[num_derivatives*neuron_index:num_derivatives*neuron_index+num_derivatives])
                     )
-			if current_neuron.child != None: current_neuron = current_neuron.child
-			else: break
-			neuron_index += 1
-		return derivatives
+            if current_neuron.child != None:
+                current_neuron = current_neuron.child
+            else:
+                break
+            neuron_index += 1
+        return derivatives
 
-	def get_starting_point(self): 
-		return (0, [position for i, neuron in enumerate(self.neurons) for position in neuron.initial_positions[1]])
+    def get_starting_point(self):
+        return (0, [position for i, neuron in enumerate(self.neurons) for position in neuron.initial_positions[1]])
--- src/biological_neuron.py
+++ src/biological_neuron.py
@@ -1,6 +1,6 @@
 from euler_estimator import EulerEstimator
 import math
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 class BiologicalNeuron:
     def __init__(self, stimulus=lambda t: 0):
@@ -12,10 +12,10 @@ class BiologicalNeuron:
         self.C, self.V_Na, self.V_k, self.V_L, self.g_bar_Na, self.g_bar_k, self.g_bar_l = 1, 115, -12, 10.6, 120, 36,  0.3
         self.child = None
 
-  #  def plot_activity(self):
-     #   euler = EulerEstimator(self.derivatives, self.initial_positions)
-      #  plt.plot([n/2 for n in range(160)], [self.stimulus(n/2) for n in range(160)])
-       # euler.plot([0, 80], stepsize =0.02)
+    def plot_activity(self):
+        euler = EulerEstimator(self.derivatives, self.initial_positions)
+        plt.plot([n/2 for n in range(160)], [self.stimulus(n/2) for n in range(160)])
+        euler.plot([0, 80], stepsize =0.02)
 
     def dV(self, t, x): return 1/self.C * (self.stimulus(t) - self.I_Na(t, x) - self.I_k(t, x) - self.I_L(t, x))
     def dn(self, t, x): return self.a_n(t, x) * (1 - x[1]) - self.b_n(t, x) * x[1]
@@ -33,7 +33,6 @@ class BiologicalNeuron:
     def g_k(self, t, x): return self.g_bar_k * x[1] ** 4
     def I_L(self, t, x): return self.g_l(t, x) * (x[0] - self.V_L)
     def g_l(self, t, x): return self.g_bar_l
-
 
 def stimulus(t):
     if 10 <= t <= 11 or 20 <= t <= 21 or 30 <= t <= 40 or 50 <= t <= 51 or 53 <= t <= 54 or 56 <= t <= 57 or 59 <= t <= 60 or 62 <= t <= 63 or 65 <= t <= 66: return 150
