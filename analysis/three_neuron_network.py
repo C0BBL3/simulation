e60142687f49b8b1499baa79c9739dce9b6c3031
Colby Roberts <colbiverse@gmail.com>
2020-11-10 20:37:36 -0800

    Update three_neuron_network.py

--- analysis/three_neuron_network.py
+++ analysis/three_neuron_network.py
@@ -1,29 +1,28 @@
 class BiologicalNeuralNetwork:
     def __init__(self, neurons, synapses):
         self.neurons = neurons
+        for i, neuron in enumerate(neurons):
+            neuron.index = i
         self.synapses = synapses
         for neuron_1_index, neuron_2_index in self.synapses:
-            self.neurons[neuron_1_index].child = self.neurons[neuron_2_index]
+            self.neurons[neuron_2_index].parent = self.neurons[neuron_1_index]
 
     def get_derivatives(self, delay=2):
-        derivatives, current_neuron, neuron_index = [], self.neurons[0], 0
-        while True:
+        derivatives, current_neuron, neuron_index = [], self.neurons[-1], 0
+        for current_neuron in self.neurons[::-1]:
             num_derivatives = len(current_neuron.derivatives)
             for derivative_index, derivative in enumerate(current_neuron.derivatives):
                 if derivative_index == 0 and neuron_index > 0:
                     derivatives.append(
-                        lambda t, x, neuron_index=neuron_index, derivative=derivative:
-                            derivative(t, x[num_derivatives*neuron_index:num_derivatives*neuron_index + num_derivatives]) + x[num_derivatives*(neuron_index-1)] if x[num_derivatives*(neuron_index-1)] * current_neuron.C > 50
-                            else derivative(t, x[num_derivatives*neuron_index:num_derivatives*neuron_index + num_derivatives]))
+                        lambda t, x, neuron_index=current_neuron.index, derivative=derivative:
+                            derivative(t, x[num_derivatives*current_neuron.index:num_derivatives*current_neuron.index 
+                            + num_derivatives]) + x[num_derivatives*current_neuron.parent.index] 
+                            if x[num_derivatives*current_neuron.parent.index] > 50
+                            else derivative(t, x[num_derivatives*current_neuron.index:num_derivatives*current_neuron.index + num_derivatives]))
                 else:
                     derivatives.append(
                         lambda t, x, neuron_index=neuron_index, derivative=derivative:
-                            derivative(t, x[num_derivatives*neuron_index: num_derivatives*neuron_index + num_derivatives]))
-            if current_neuron.child != None:
-                current_neuron = current_neuron.child
-            else:
-                break
-            neuron_index += 1
+                            derivative(t, x[num_derivatives*current_neuron.index: num_derivatives*current_neuron.index + num_derivatives]))
         return derivatives
 
     def get_starting_point(self):
